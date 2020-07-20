4c6617e0dfc785903b359044082f7fb9f36aba8f
Matthew Lean <matt@mattlean.com>
2020-07-20 14:48:34 -0700

    Update project config - Reinforce style rule order with stylelint-order - Update build to include declaration files, source maps and JSX

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,18 @@
 const del = require('del')
 const { dest, series, src } = require('gulp')
+const {
+  init: sourceMapsInit,
+  write: sourceMapsWrite,
+} = require('gulp-sourcemaps')
 const { createProject } = require('gulp-typescript')
-const tsProject = createProject('tsconfig.json')
+
+const tsProject = createProject('tsconfig.json', {
+  declaration: true,
+  isolatedModules: false,
+  jsx: 'preserve',
+  noEmit: false,
+  sourceMap: true,
+})
 
 const clean = () => del(['dist/**/*'])
 
@@ -14,7 +25,9 @@ const compileTS = () =>
     '!src/**/*.test.ts',
     '!src/setupTests.ts',
   ])
+    .pipe(sourceMapsInit())
     .pipe(tsProject())
+    .pipe(sourceMapsWrite())
     .pipe(dest('dist'))
 
 const copyStyles = () =>
